8fa70dcf5128566ad2e3349561d97bf7d44591ca
hsobol <hsobol@hotmail.com>
2017-03-31 23:57:11 +0200

    Delete CollectionIndirectExtendingGenericsPojo.java

--- src/test/dto/CollectionIndirectExtendingGenericsPojo.java
+++
@@ -1,5 +0,0 @@
-package uk.co.jemos.podam.test.dto;
-
-public class CollectionIndirectExtendingGenericsPojo extends CollectionExtendingGenericsPojo {
-	private static final long serialVersionUID = 1L;
-}
